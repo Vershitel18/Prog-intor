aecd8f692166c4f1f7edaff50eabdc9e3b305561
Vologdin Nikolay Danilovich <99263613+Vershitel18@users.noreply.github.com>
2026-07-31 12:01:50 +0300

    Delete Discra_1_curs directory

--- Discra_1_curs/LZW.py
+++
@@ -1,5 +0,0 @@
-n = input()
-A = list()
-for i in range(len(n)):
-    if A.count(n[i]) == 0:
-        A.append(n[i])
--- Discra_1_curs/anti_baroz.py
+++
@@ -1,9 +0,0 @@
-n = input()
-A = list(n)
-for i in range(len(n)-1):
-    A = sorted(A)
-    for j in range(len(n)):
-        A[j] = n[j] + A[j]
-A = sorted(A)
-print(A[0])
-
